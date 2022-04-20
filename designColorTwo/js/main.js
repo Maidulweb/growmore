d9dfb563277ced1a0e43f49dc626130b4b5c15ca
Maidulweb <maidulimv@gmail.com>
2022-04-20 16:55:18 +0600

    background color2

---
+++ designColorTwo/js/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+    $('.menu-bar').click(function (e) {
+
+        // Used to stop the event bubbling..
+        e.stopPropagation()
+        $('.menu-list').toggle();
+    });
+    
+    // Hide the "info_image_click" by clicking outside container
+    $(document).click(function () {
+        $('.menu-list').hide();
+    });
+    $('.menu-bar').hover(function (e) {
+
+        // Used to stop the event bubbling..
+        e.stopPropagation()
+        $('.menu-list').toggle();
+    });
+    
+    // Hide the "info_image_click" by clicking outside container
+    $(document).hover(function () {
+        $('.menu-list').hide();
+    });
+  });
